752e3c84910c33af1c276faa151040d184104779
svg3982 <simone_v.g@hotmail.com>
2022-11-27 22:40:38 +0000

    part 2

--- dec9.py
+++ dec9.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas
 
 heights = np.loadtxt("dec9_test_input.txt",dtype=str)
-heights_p = pandas.read_csv("dec9_test_input.txt", dtype=str, header=None)
-
-heights_p = pandas.DataFrame([list(x) for x in heights_p[0]])
-print(heights_p)
+heights_columns = np.genfromtxt("dec9_test_input.txt",dtype=str,delimiter=1)
+print(heights_columns[:,0])
 
 def check_up_down(value, position,row):
     if row == 0: up = 9
@@ -40,3 +38,40 @@ for row,location in enumerate(heights): #loop through every line in input file (
                 risks.append(1+int(height_value))
         left = current
 print(sum(risks))
+
+basin_sizes = []
+# loop through points to calculate size of each basin
+for row in min_positions_rows:
+    for point in min_positions_rows.get(row):
+        print(row,point)
+        #find 9s in current row
+        row_no = int(row)
+        full_string = heights[row_no]
+        left_limit = full_string.rfind("9", 0, point)
+        right_limit = full_string.find("9", point)
+        if right_limit == -1: right_limit = len(full_string) #find() puts -1 if not found
+        basin_width = range(left_limit+1, right_limit)
+        basin_temp = 0
+        #find 9s in column
+        for count,i in enumerate(basin_width):
+            col = heights_columns[:,i] #row_no = current row
+            if row_no == 0: upper_limit = 0
+            else: 
+                stop = np.where(col[:row_no] == "9")
+                if any(map(len, stop)): #check whether stop (tuple) is not empty
+                    upper_limit = stop[0][-1]+1
+                else: 
+                    upper_limit = len(heights)
+            if row_no == len(heights)-1: lower_limit = len(heights) 
+            else: 
+                stop = np.where(col[row_no:] == "9")
+                if any(map(len, stop)):
+                    lower_limit = (row_no + stop[0][0])
+                else:
+                    lower_limit = len(heights) #might also be row_no... check
+            basin_temp = basin_temp + (lower_limit - upper_limit)
+            #find stragglers
+        basin_sizes.append(basin_temp)
+        print(basin_sizes)
+        print(3+9+13+9)
+        print(np.count_nonzero(heights_columns[:,:] == "9"))
